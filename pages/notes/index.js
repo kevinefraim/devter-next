bca911d6f301eecf388be4562bcb93678d4771bb
kevinefraim <kevinefraim02@gmail.com>
2022-05-12 14:26:19 -0300

    validation data

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,10 +1,12 @@
 import axios from "axios";
+import Link from "next/link";
 import React from "react";
 import AppLayout from "../../components/AppLayout";
 import Navbar from "../../components/Navbar";
 import NoteCard from "../../components/NoteCard";
 
 const Notes = ({ data }) => {
+  console.log(data.length);
   return (
     <AppLayout>
       <Navbar />
@@ -13,11 +15,22 @@ const Notes = ({ data }) => {
           Notes
         </h1>
       </div>
-      <div className="grid grid-cols-5 mt-10 ml-10 gap-4">
-        {data?.map((note) => {
-          return <NoteCard key={note._id} note={note} />;
-        })}
-      </div>
+      {data.length === 0 ? (
+        <h2 className="text-center text-red-500 mt-10 font-extrabold text-5xl">
+          No hay notas guardadas,{" "}
+          <Link href="/new">
+            <a className="text-blue-500 underline hover:underline hover:text-gray-300">
+              agregue una nueva
+            </a>
+          </Link>
+        </h2>
+      ) : (
+        <div className="grid grid-cols-5 mt-10 ml-10 gap-4">
+          {data?.map((note) => {
+            return <NoteCard key={note._id} note={note} />;
+          })}
+        </div>
+      )}
     </AppLayout>
   );
 };
